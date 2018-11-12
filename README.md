b1c3f6ede3329a6cd5bc8f1e5c99bd8d71c28075
Roman Kuzmenko <kuzmrom7@gmail.com>
2018-11-13 01:12:04 +0300

    add dockerfile, simple test and travis config

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 ## Trains
+
+## Run
+
+```
+docker build -t trains-client .
+docker run -d \
+  -v ${PWD}:/usr/src/app \
+  -v /usr/src/app/node_modules \
+  -p 3000:3000 \
+  --rm \
+  trains-client
+```
--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../App';
+import React from "react";
+import ReactDOM from "react-dom";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+import { BackgroundImage } from "../components/Background";
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<BackgroundImage />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
